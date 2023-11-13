5af559d1b17f4240b0d3deb2e36dd5160b69a0a0
EstanisEVL <estanislaovl@gmail.com>
2023-11-13 16:30:17 -0300

    feat: Add styling and JS functionalities

--- js/app.js
+++ js/app.js
@@ -1,1 +1,39 @@
-console.log("Hello World!");
+const container = document.querySelector(".container");
+const newGridBtn = document.querySelector(".btn-new");
+const clearGridBtn = document.querySelector(".btn-clear");
+
+let mouseDown = false;
+document.body.onmousedown = () => {mouseDown = true};
+document.body.onmouseup = () => {mouseDown = false};
+
+const clearGrid = () => {
+  container.innerHTML = "";
+}
+
+const createGrid = () => {
+  clearGrid();
+
+  let gridSize = prompt("How many squares per side for the new grid? MAX SIZE ALLOWED: 64");
+
+  if(gridSize > 64) return;
+
+  container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+  container.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
+
+  for(let i = 1; i <= Number(gridSize * gridSize); i++) {
+    const div = document.createElement("div");
+    div.classList.add("div");
+    div.addEventListener("mouseover", changeColor);
+    div.addEventListener("mousedown", changeColor);
+    container.appendChild(div);
+  }
+}
+
+const changeColor = (e) => {
+  if(e.type === "mouseover" && !mouseDown) return;
+
+  e.target.style.backgroundColor = "black";
+}
+
+newGridBtn.addEventListener("click", createGrid);
+clearGridBtn.addEventListener("click", clearGrid);
